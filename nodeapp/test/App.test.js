ee70603dc450391e64925c519bdba71b87f09ec7
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:53:11 +0000

    Backup Commit

--- nodeapp/test/App.test.js
+++ nodeapp/test/App.test.js
@@ -23,7 +23,7 @@ const app = require('../app'); // Import your Express app instance
 describe('API Tests', () => {
  
   it('should_get_a_list_of_items', async () => {
-    const response = await request(app).get('/items');
+    const response = await request("http://api.example.com/").get('/items');
     expect(response.status).toBe(200);
     expect(response.body).toHaveLength(2); // Check the length of the array
   });
